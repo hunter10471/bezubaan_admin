35074194f923a8f7abde5556c1da95da068cedec
Rafay Zia <rafayzia10@gmail.com>
2023-07-06 19:58:37 +0500

    Setup next app with tailwindCss

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,8 @@
+'use client';
+import React from 'react';
+
+const Sidebar = () => {
+	return <aside className='w-[200px] py-6 px-4 '>Sidebar</aside>;
+};
+
+export default Sidebar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				heading: '#127357',
+				primary: '#40B37C',
+				secondary: '#1ED96F',
+				text: '#40B37C',
+				subheading: '#010B40',
+				white: '#fff',
+				black: '#000',
+				gray: '#535256',
+				accent: '#DA6525',
+				lightBlack: '#454545',
+			},
+		},
+	},
+};
